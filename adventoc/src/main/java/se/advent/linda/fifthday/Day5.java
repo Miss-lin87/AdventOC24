f33ba035b844ceba009d647e62a9a6d83c673102
linda <linda.ek@bredband2.com>
2024-12-05 14:38:11 +0100

    fixed indentations, day5

--- adventoc/src/main/java/se/advent/linda/fifthday/Day5.java
+++ adventoc/src/main/java/se/advent/linda/fifthday/Day5.java
@@ -38,49 +38,41 @@ public class Day5 {
     }
 
     boolean test_valid_one(List<String> rules, String input){
-        boolean test_one;
-        test_one = rules.contains(input);
+        boolean test_one = rules.contains(input);
         return test_one;
     }
 
     boolean test_valid_all(List<String> rules , String[] input){
         int counter = 0;
-        String number_pair = "";
+        String number_pair;
         boolean test = true;
         for (String elem : input) {
             for (int i = 1; i < input.length-counter; i++) {
                 number_pair = (elem + "|" + input[i+counter]);
-                //System.out.println(number_pair);
                 if(test_valid_one(rules, number_pair)==false){
                     test = false;
                     break;
                 }
             }
             counter ++;
-            //System.out.println("");
         }
-        //System.out.println(test);
         return test;
     }
 
     int find_midle_number(String[] input){
-        int midle = input.length/2;
-        int output = Integer.parseInt(input[midle]);
+        int output = Integer.parseInt(input[input.length/2]);
         return output;
     }
     public static void main(String[] args) {
         Day5 day = new Day5();
         int total = 0;
         List<String> rules = day.get_rulings_list("");
-        //System.out.println(rules);
         List<String> input = day.get_raw_data("");
         for (int i = 0; i < input.size(); i++) {
             String[] temp = input.get(i).split("\\|");
-            //System.out.println(Arrays.toString(input.get(i).split("\\|")));
             if (day.test_valid_all(rules, temp) == true){
                 total += day.find_midle_number(temp);
             }
-            
         }
         System.out.println(total);
     }
--- adventoc/src/main/java/se/advent/linda/fifthday/Day5t2.java
+++ adventoc/src/main/java/se/advent/linda/fifthday/Day5t2.java
@@ -11,22 +11,18 @@ public class Day5t2{
                 input_to_sort[i] = input_to_sort[i+1];
                 input_to_sort[i+1] = temp;
             }
-            
         }
         return input_to_sort;
     }
 
     String[] sort_to_valid(List<String> rules, String[] sorted_list){
         Day5 day = new Day5();
-        //System.err.println(Arrays.toString(sorted_list));
         for (int i = sorted_list.length-1; i > 0; i--) {
-            String temp = sorted_list[i-1] + "|" +  sorted_list[i];
-            if (day.test_valid_one(rules, temp) == false){
+            if (day.test_valid_one(rules, (sorted_list[i-1] + "|" +  sorted_list[i])) == false){
                 String placeholder;
                 placeholder = sorted_list[i];
                 sorted_list[i] = sorted_list[i-1];
                 sorted_list[i-1] = placeholder;
-                //System.err.println(Arrays.toString(sorted_list));
                 sort_to_valid(rules, sorted_list);
             }
         }
@@ -40,10 +36,8 @@ public class Day5t2{
         List<String> input = day.get_raw_data("");
         for (int i = 0; i < input.size(); i++) {
             String[] temp = input.get(i).split("\\|");
-            //System.out.println(Arrays.toString(input.get(i).split("\\|")));
             if (day.test_valid_all(rules, temp) == false){
-                temp = day2.sort_list(temp);
-                temp = day2.sort_to_valid(rules, temp);
+                temp = day2.sort_to_valid(rules, day2.sort_list(temp));
                 counter += day.find_midle_number(temp);
             }
         }
